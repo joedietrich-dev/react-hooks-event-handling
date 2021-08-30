ff009fc64701ea1454bab68b6d6b858fb1f1daff
Joe Dietrich <joe@joedietrich.net>
2021-08-29 22:06:32 -0400

    commit tree 7941e60388e06a0b27bfb1e3de9092301c759bb0

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function Tickler() {
-  function tickle() {
+  function tickle(e) {
     console.log("Teehee!");
+    console.log(e);
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
 
 export default Tickler;
